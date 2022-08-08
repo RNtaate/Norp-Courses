eff2e3efb8cc3feadad1c50f1eae4d443bb52604
RNtaate <royhyde14@gmail.com>
2022-08-08 18:29:05 +0300

    Update index, create actions in courses controller.
    Update index.html.erb for courses to reflect user email.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,7 @@ class CoursesController < ApplicationController
 
   # GET /courses or /courses.json
   def index
-    @courses = Course.all.order('created_at DESC')
+    @courses = Course.all.includes(:user).order('created_at DESC')
   end
 
   # GET /courses/1 or /courses/1.json
@@ -21,7 +21,8 @@ class CoursesController < ApplicationController
 
   # POST /courses or /courses.json
   def create
-    @course = Course.new(course_params)
+    #current user creates the courses.
+    @course = current_user.courses.build(course_params)
 
     respond_to do |format|
       if @course.save
