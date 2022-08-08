0e1292d5eff0522511e53d80639f41c97c52d838
RNtaate <royhyde14@gmail.com>
2022-08-08 18:08:19 +0300

    Generate new migration to add user to courses
    Seed data for it.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: "ironman@marvel.com", password: "123456", password_confirmation: "123456")
+
 30.times do 
   Course.create!(
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::BigBangTheory.quote
+    description: Faker::TvShows::BigBangTheory.quote,
+    user_id: User.first.id
   )
 end
