8e3effc8595b647d3f76118e7cf255f0c4118a18
RNtaate <royhyde14@gmail.com>
2022-08-10 02:15:09 +0300

    Add another section to the index.html page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @courses = Course.all.limit(3)
+    @courses = Course.all
     @latest_courses = Course.all.limit(3).order(created_at: :desc);
   end
 end
