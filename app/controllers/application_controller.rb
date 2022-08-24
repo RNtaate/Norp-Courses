2e3a889ef875e73d5daf4bd6704de1e2366d2632
RNtaate <royhyde14@gmail.com>
2022-08-25 02:47:48 +0300

    Update before action in application controller to fix access to courses after signing out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  before_action :set_global_variables,  if: :user_signed_in?
+  before_action :set_global_variables
 
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
