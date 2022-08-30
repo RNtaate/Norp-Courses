fbcb558c0aed5a0f4963bf1a2847133aed0c459f
RNtaate <royhyde14@gmail.com>
2022-08-30 11:28:38 +0300

    Run command 'rails g rolify Role User' to add rolify to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
   
+  rolify
+
   has_many :courses
 
   def username
