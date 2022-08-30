475c6ebf889623c5116d5233fa76e060af3f2ce8
RNtaate <royhyde14@gmail.com>
2022-08-31 00:17:50 +0300

    Include pundit module and the rescue from unauthorized  access code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
 
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :authenticate_user!
 
   before_action :set_global_variables
 
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
   end
 end
