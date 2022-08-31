590f6dcc4106a49f48ca2505034a4fb68d698218
RNtaate <royhyde14@gmail.com>
2022-08-31 20:38:19 +0300

    Create user_activity function, use it in after_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
 
   before_action :set_global_variables
 
+  after_action :user_activity
+
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
   end
@@ -18,5 +20,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_back(fallback_location: root_path)
+  end
+
+  def user_activity
+    current_user.try :touch
   end
 end
