ec6fe932f3f2a9d6ff687af7e7aecf021bdef36b
RNtaate <royhyde14@gmail.com>
2022-09-05 23:29:08 +0300

    Update lesson controller actions with pundit authorization
    Hide some partial buttons using pundit lesson policy

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,6 +9,7 @@ class LessonsController < ApplicationController
 
   # GET /lessons/1 or /lessons/1.json
   def show
+    authorize @lesson
   end
 
   # GET /lessons/new
@@ -18,6 +19,7 @@ class LessonsController < ApplicationController
 
   # GET /lessons/1/edit
   def edit
+    authorize @lesson
   end
 
   # POST /lessons or /lessons.json
@@ -39,6 +41,7 @@ class LessonsController < ApplicationController
 
   # PATCH/PUT /lessons/1 or /lessons/1.json
   def update
+    authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
         format.html { redirect_to course_lesson_url(@course, @lesson), notice: "Lesson was successfully updated." }
@@ -52,6 +55,7 @@ class LessonsController < ApplicationController
 
   # DELETE /lessons/1 or /lessons/1.json
   def destroy
+    authorize @lesson
     @lesson.destroy
 
     respond_to do |format|
